86e0d0a0a8556411f426fdc340e92a4af279959c
Ellen Marie Dash <me@duckie.co>
2020-05-03 14:27:27 -0400

    Simplify/refactor test/util.py.

--- test/util.py
+++ test/util.py
@@ -3,16 +3,16 @@ import subprocess
 class TestResult:
     def __init__(self, result):
         self.raw_result = result
+        self.returncode = result.returncode
         self.stdout = result.stdout.decode()
         self.stderr = result.stderr.decode()
 
 def run(args, should_fail=False):
     result = subprocess.run(args, capture_output=True)
-    if should_fail:
-        assert result.returncode > 0
-    else:
-        assert result.returncode == 0
+    assert result.returncode == 0
     return TestResult(result)
 
 def fails(args):
-    return run(args, should_fail=True)
+    result = subprocess.run(args, capture_output=True)
+    assert result.returncode > 0
+    return TestResult(result)
