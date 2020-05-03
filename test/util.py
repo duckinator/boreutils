df579933fe6df88dd7e550147de5cd1d2b864324
Ellen Marie Dash <me@duckie.co>
2020-05-03 15:09:27 -0400

    Vastly simplify test/util.py; update test/test_basename.py accordingly.

--- test/util.py
+++ test/util.py
@@ -1,18 +1,7 @@
 import subprocess
 
-class TestResult:
-    def __init__(self, result):
-        self.raw_result = result
-        self.returncode = result.returncode
-        self.stdout = result.stdout.decode()
-        self.stderr = result.stderr.decode()
+def check(args):
+    return subprocess.run(args, capture_output=True, text=True, check=True)
 
-def run(args, should_fail=False):
-    result = subprocess.run(args, capture_output=True)
-    assert result.returncode == 0
-    return TestResult(result)
-
-def fails(args):
-    result = subprocess.run(args, capture_output=True)
-    assert result.returncode > 0
-    return TestResult(result)
+def run(args):
+    return subprocess.run(args, capture_output=True, text=True)
