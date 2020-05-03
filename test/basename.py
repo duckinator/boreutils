d4925f6256abb7ce05032a6f33cfcac2d70a17c4
Ellen Marie Dash <me@duckie.co>
2020-05-03 14:02:21 -0400

    I have no idea what I'm doing.

--- test/basename.py
+++ test/basename.py
@@ -1,36 +1,37 @@
-from .util import prints_help, run, fails
+from .util import *
+#from .util import prints_help, run, fails
 from pathlib import Path
 
 # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/basename.html
 
 basename = "./bin/basename"
 
-assert run([basename, "--version"]).stdout.decode().split(' ')[0] == 'Boreutils'
+assert_eq(run([basename, "--version"]).stdout.split(' ')[0], 'Boreutils')
 
 # No args => error of the form "basename: ..." or "/path/to/basename: ..."
-assert fails([basename]).stderr.decode().split(' ')[0].endswith("basename:")
+assert_true(fails([basename]).stderr.split(' ')[0].endswith("basename:"))
 
 # Test the various steps:
 
 ## 1. Empty string results in an empty string.
-assert run([basename, ""]).stdout.decode().strip() == ""
+assert_eq(run([basename, ""]).stdout.strip(), "")
 
 ## 2. We _do not_ skip steps 3-6 if given "//", so this should return "/".
 ##    If we do skip step 3-6, this should return "//"!
-assert run([basename, "//"]).stdout.decode().strip() == "/"
+assert_eq(run([basename, "//"]).stdout.strip(), "/")
 
 ## 3. If string is entirely slash characters, we get a single slash.
-assert run([basename, "///"]).stdout.decode().strip() == "/"
+assert_eq(run([basename, "///"]).stdout.strip(), "/")
 
 ## 4. Remove trailing slash characters.
-assert run([basename, "owo/"]).stdout.decode().strip() == "owo"
+assert_eq(run([basename, "owo/"]).stdout.strip(), "owo")
 #! Potential edge case if we change behavior for step 2.
-assert run([basename, "owo//"]).stdout.decode().strip() == "owo"
-assert run([basename, "owo///"]).stdout.decode().strip() == "owo"
+assert_eq(run([basename, "owo//"]).stdout.strip(), "owo")
+assert_eq(run([basename, "owo///"]).stdout.strip(), "owo")
 
 ## 5. If there are remaining slash characters, remove everything up to and
 ##    including the last slash.
-assert run([basename, "/a/b/c/d/owo"]).stdout.decode().strip() == "owo"
+assert_eq(run([basename, "/a/b/c/d/owo"]).stdout.strip(), "owo")
 #! Potential edge case exercising steps 4+5 together.
-assert run([basename, "/a/b/c/d/owo///"]).stdout.decode().strip() == "owo"
-assert run([basename, "///a/b/c/d/owo///"]).stdout.decode().strip() == "owo"
+assert_eq(run([basename, "/a/b/c/d/owo///"]).stdout.strip(), "owo")
+assert_eq(run([basename, "///a/b/c/d/owo///"]).stdout.strip(), "owo")
--- test/util.py
+++ test/util.py
@@ -1,12 +1,30 @@
 import subprocess
 
+class TestResult:
+    def __init__(self, result):
+        self.raw_result = result
+        self.stdout = result.stdout.decode()
+        self.stderr = result.stderr.decode()
+
+class TestFailure(Exception):
+    pass
+
+def assert_eq(actual, expected):
+    if expected != actual:
+        # FIXME: Make this less of a disaster.
+        message = f"\n\nExpected:\n    {expected!r}\nGot:\n    {actual!r}"
+        raise TestFailure(message)
+
+def assert_true(actual):
+    assert_eq(actual, True)
+
 def run(args, should_fail=False):
     result = subprocess.run(args, capture_output=True)
     if should_fail:
         assert result.returncode > 0
     else:
         assert result.returncode == 0
-    return result
+    return TestResult(result)
 
 def fails(args):
     return run(args, should_fail=True)
