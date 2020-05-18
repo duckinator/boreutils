206715a3f21e7cf8d3a45bf5b1a5946aed5eddbf
Ellen Marie Dash <me@duckie.co>
2020-05-17 23:45:54 -0400

    fix failing tests.

--- src/true.c
+++ src/true.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
     if (has_arg(argc, argv, "-h") || has_arg(argc, argv, "--help")) {
         printf("Usage: %s\n\n", argv[0]);
         printf("Do nothing and succeed.\n");
+        return 1;
     }
 
     if (bu_handle_version(argc, argv)) {
