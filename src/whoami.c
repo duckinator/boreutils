e8402fd9164c35270b7c3a65f29d4f2bbe04065d
Ellen Marie Dash <me@duckie.co>
2020-05-19 17:43:23 -0400

    Clean up whoami and add tests.

--- src/whoami.c
+++ src/whoami.c
@@ -7,7 +7,16 @@ int main(int argc, char **argv)
     if (has_arg(argc, argv, "-h") || has_arg(argc, argv, "--help")) {
         printf("Usage: %s\n\n", argv[0]);
         printf("Print effective userid.\n");
+        return 1;
+    }
+
+    if (bu_handle_version(argc, argv)) {
         return 0;
+    }
+
+    if (argc > 1) {
+        bu_extra_argument(argv[0]);
+        return 1;
     }
 
     char *ret = getlogin();
---
+++ test/test_whoami.py
@@ -0,0 +1,33 @@
+"""
+Tests for `whoami`.
+
+TODO: Determine if there's a standard for this.
+"""
+
+import os
+from helpers import check, check_version, run
+
+
+def test_checking_correct_program():
+    """Check that we're using Boreutil's implementation."""
+    assert check_version("whoami")
+
+
+def test_extra_args():
+    """Extra args => error of the form `whoami: ...`"""
+    assert run(["whoami", "owo"]).stderr.startswith("whoami: ")
+    assert run(["whoami", "owo"]).returncode > 0
+
+
+def test_help():
+    """Passing -h or --help => print help text."""
+    assert run(["whoami", "-h"]).stdout.split(' ')[0] == 'Usage:'
+    assert run(["whoami", "--help"]).stdout.split(' ')[0] == 'Usage:'
+    assert run(["whoami", "-h"]).returncode > 0
+    assert run(["whoami", "--help"]).returncode > 0
+
+
+def test_main():
+    """No args => user name associated with the current effective user ID."""
+    assert check(["whoami"]).stdout == os.getlogin() + "\n"
+    assert len(check(["whoami"]).stderr) == 0
