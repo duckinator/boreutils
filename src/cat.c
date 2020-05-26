034c42857c59daad7abfa0ca849113406d866053
Ellen Marie Dash <me@duckie.co>
2020-05-26 19:17:26 -0400

    Add `cat`.

---
+++ src/cat.c
@@ -0,0 +1,87 @@
+/**
+ * NAME
+ * ====
+ *     cat - concatenate files and print them
+ *
+ * SYNOPSIS
+ * ========
+ *     cat [-u] FILE...
+ *     cat [--help|--version]
+ *
+ * DESCRIPTION
+ * ===========
+ *     Concatenate files and print them to standard output.
+ *
+ *     -u           Don't buffer the output.
+ *
+ *     FILE         The file(s) to print.
+ *
+ *     --help       Print help text and exit.
+ *     --version    Print version information and exit.
+ */
+
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "boreutils.h"
+
+
+static int cat_fd(int fd) {
+    // Size is arbitrary; feel free to adjust it if needed.
+    char buf[4096] = {0};
+    ssize_t bytes_read = 1;
+    while (bytes_read > 0) {
+        bytes_read = read(fd, buf, sizeof(buf) - 1);
+        buf[bytes_read] = 0;
+        fputs(buf, stdout);
+    }
+
+    if (bytes_read == -1) {
+        perror("cat: error reading file");
+        return 1;
+    }
+
+    return 0;
+}
+
+static int cat_file(char *path) {
+    int fd = open(path, O_RDONLY);
+
+    if (fd == -1) {
+        perror("cat: error opening file");
+        return 1;
+    }
+
+    return cat_fd(fd);
+}
+
+int main(int argc, char **argv)
+{
+    if (has_arg(argc, argv, "-h") || has_arg(argc, argv, "--help")) {
+        puts("Usage: cat [-u] FILE...");
+        puts("");
+        puts("Concatenate and print the specified file(s).");
+        puts("");
+        puts("FILE    The file(s) to print.");
+        return 1;
+    }
+
+    if (bu_handle_version(argc, argv)) {
+        return 0;
+    }
+
+    if (argc < 2) {
+        return cat_fd(STDIN_FILENO);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int ret = cat_file(argv[i]);
+        if (ret != 0) {
+            return ret;
+        }
+    }
+
+    return 0;
+}
---
+++ test/test_cat.py
@@ -0,0 +1,56 @@
+"""
+Tests for POSIX-compatible `cat`.
+
+https://pubs.opengroup.org/onlinepubs/9699919799/utilities/cat.html
+"""
+
+from pathlib import Path
+import subprocess
+from helpers import check, check_version, run
+
+
+def test_version():
+    """Check that we're using Boreutil's implementation."""
+    assert check_version("cat")
+
+
+def test_missing_args():
+    """Nothing to test: `cat` accepts any number of arguments."""
+    pass
+
+
+def test_extra_args():
+    """Nothing to test: `cat` accepts any number of arguments."""
+    pass
+
+
+def test_help():
+    """Passing -h or --help => print help text."""
+    assert run(["cat", "-h"]).stdout.split(' ')[0] == 'Usage:'
+    assert run(["cat", "--help"]).stdout.split(' ')[0] == 'Usage:'
+    assert run(["cat", "-h"]).returncode > 0
+    assert run(["cat", "--help"]).returncode > 0
+
+
+def test_main():
+    """Split across test_file() and test_stdin()"""
+    pass
+
+
+def test_file():
+    """Passing arguments should read from files."""
+    path1 = Path("src/basename.c").resolve()
+    path2 = Path("src/cal.c").resolve()
+    path3 = Path("src/cat.c").resolve()
+    full_results = path1.read_text() + path2.read_text() + path3.read_text()
+    assert run(["cat", str(path1)]).stdout == path1.read_text()
+    assert run(["cat", str(path1), str(path2), str(path3)]).stdout == full_results
+
+
+def test_stdin():
+    """Passing no arguments should read from stdin."""
+    p1 = subprocess.Popen(["echo", "owo"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["./bin/cat"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    output = p2.communicate()[0].decode()
+    assert output == "owo\n"
