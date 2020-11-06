6e45a305190bf3b9886f95cf743b76b956274f79
Ellen Marie Dash <me@duckie.co>
2020-11-06 17:50:38 -0500

    Improve tests for `date`; improve `date` error.

--- test/test_date.py
+++ test/test_date.py
@@ -1,11 +1,13 @@
 """
 Tests for POSIX-compatible `date`.
 
+TODO: Add tests for setting the date/time.
+
 https://pubs.opengroup.org/onlinepubs/9699919799/utilities/date.html
 """
 
 import time
-from helpers import check_version, check, run
+from helpers import check_version, check, check_fail, run
 
 
 def test_version():
@@ -14,13 +16,13 @@ def test_version():
 
 
 def test_missing_args():
-    """Nothing to test: `date` accepts any number of arguments."""
+    """Nothing to test: `date` can be given zero arguments."""
     pass
 
 
 def test_extra_args():
     """Nothing to test: `date` accepts any number of arguments."""
-    pass
+    assert len(check_fail(["date", "a", "b"]).stderr) > 0
 
 
 def test_help():
@@ -44,3 +46,7 @@ def test_main():
     # Format string for ISO 8601.
     assert check(["date", "+%FT%TZ"]).stdout == time.strftime("%FT%TZ\n")
     assert len(check(["date", "+%FT%TZ"]).stderr) == 0
+
+    # Example without a leading + sign.
+    assert len(check_fail(["date", "%H:%M:%S"]).stdout) == 0
+    assert "Invalid date string" in check_fail(["date", "%H:%M:%S"]).stderr
